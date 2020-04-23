a51bb0c604a5c375696cad05bc6e43ce5fc811f7
Sunny Golovine <sunny@glvn.co>
2020-04-23 13:51:33 -0400

    Bootstrap more files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,54 @@
 module.exports = {
-  plugins: [`gatsby-plugin-typescript`],
-};
+  siteMetadata: {
+    title: `glvn.co`,
+    description: `Website for Sunny Golovine`,
+    author: `Sunny Golovine`,
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'assets',
+        path: `${__dirname}/static/assets/`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/cms/pages/`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/cms/components/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'assets',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 2048,
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-typescript`,
+  ],
+}
