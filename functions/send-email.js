2cbfc9c2720a350cf5c50fc235b6ac10320481da
Sunny Golovine <sunny@glvn.co>
2020-04-27 00:00:28 -0400

    Fixed sending messages in contact form

--- functions/send-email.js
+++ functions/send-email.js
@@ -1,23 +1,22 @@
-const mail = require("@sendgrid/mail")
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const mail = require('@sendgrid/mail')
 
 const { SENDGRID_API_KEY, REPLY_TO_EMAIL } = process.env
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 module.exports.handler = async function (event) {
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method not Allowed" }
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method not Allowed' }
   }
 
   const params = JSON.parse(event.body)
-
-  console.log(params)
 
   const { name, email, message } = params
 
   mail.setApiKey(SENDGRID_API_KEY)
   const msg = {
     to: REPLY_TO_EMAIL,
-    from: "sunny@glvn.co",
-    subject: "glvn.co - New Message",
+    from: 'sunny@glvn.co',
+    subject: 'glvn.co - New Message',
     text: `
       Message from ${name}<${email}>
 
@@ -30,8 +29,9 @@ module.exports.handler = async function (event) {
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: "there was an error" }),
+      body: JSON.stringify({ message: 'there was an error' }),
     }
   } finally {
-    return { statusCode: 204, body: JSON.stringify({ message: "success" }) }
+    return { statusCode: 204, body: JSON.stringify({ message: 'success' }) }
+  }
 }
