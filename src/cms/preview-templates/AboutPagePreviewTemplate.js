251ef4d5c2675afd13a6bb1ad0731696f89f12cc
Sunny Golovine <sunny@glvn.co>
2020-04-28 21:56:16 -0400

    Remove console log

--- src/cms/preview-templates/AboutPagePreviewTemplate.js
+++ src/cms/preview-templates/AboutPagePreviewTemplate.js
@@ -2,7 +2,6 @@ import React from 'react'
 // import { Page } from '../../pages/about'
 
 const AboutPagePreviewTemplate = ({ entry }) => {
-  console.log(entry.getIn(['data']))
   return <h1>Check your console</h1>
 }
 
