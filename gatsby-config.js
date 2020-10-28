0e4b29d4d6678e5d7279dcb13e69947d6634d2d3
Sunny Golovine <sunny@glvn.co>
2020-10-27 23:14:29 -0400

    Add matomo tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,15 @@ module.exports = {
     description: `Website for Sunny Golovine`,
     author: `Sunny Golovine`,
   },
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-matomo`,
+      options: {
+        siteId: `2`,
+        matomoUrl: `https://stats.glvn.co/matomo`,
+        siteUrl: `https://glvn.co`,
+      },
+    },
+  ],
 }
